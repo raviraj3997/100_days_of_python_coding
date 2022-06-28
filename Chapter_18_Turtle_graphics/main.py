d688521840b9451cd32c2c69c3fb0e130dc46015
raviraj3997@gmail.com <raviraj3997@gmail.com>
2022-06-28 22:34:59 +0530

    Updating the turtle graphics code 28-06-2022
    Signed-off-by: raviraj3997@gmail.com <raviraj3997@gmail.com>

--- Chapter_18_Turtle_graphics/main.py
+++ Chapter_18_Turtle_graphics/main.py
@@ -1,16 +1,58 @@
-# This is a sample Python script.
+from turtle import Turtle, Screen
+import random
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+tim = Turtle()
+screen = Screen()
+tim.shape('turtle')
+tim.color("green")
+color_list = ['alice blue', 'AliceBlue', 'antique white', 'AntiqueWhite', 'AntiqueWhite4', 'aquamarine', 'aquamarine1',
+              'azure', 'azure1', 'azure2', 'beige', 'bisque', 'bisque3', 'bisque4', 'black', 'blanched almond',
+              'BlanchedAlmond', 'blue', 'blue violet', 'blue1', 'blue4', 'BlueViolet', 'brown', 'brown1',
+              'brown4', 'burlywood', 'burlywood3']
+# ----------------------------------
+# draw the square.
+# ----------------------------------
+
+# screen.clear()
+for _ in range(4):
+    tim.forward(100)
+    tim.lt(90)
+
+# ----------------------------------
+# draw the dashed lines.
+# ----------------------------------
+
+# screen.clear()
+for _ in range(4):
+    for _ in range(10):
+        tim.forward(10)
+        tim.penup()
+        tim.forward(10)
+        tim.pendown()
+
+    tim.lt(90)
+
+# ----------------------------------
+# drawing different Shapes.
+# ----------------------------------
+# screen.clear()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def draw_shape(num_edge):
+    angle = 360/num_edge
+    tim.color(random.choice(color_list))
+    for i in range(num_edge):
+        tim.forward(100)
+        tim.rt(angle)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+for number_edges in range(3, 20):
+    draw_shape(number_edges)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# ----------------------------------
+# drawing Random Movement of the turtle.
+# ----------------------------------
+
+
+screen.exitonclick()
